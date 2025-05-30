e55004b7f6ba15b2b68ae7be9d4f293855a73fa4
Isaiah Ozadhe <tekkieware@gmail.com>
2025-05-30 10:36:46 +0100

    trigger redeploy after adding environment variable

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -356,7 +356,6 @@ export default function GamePage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      {/* Toast notifications */}
       <LevelCompleteToast show={showSuccessToast} level={currentStage + 1} score={score} />
 
       <ErrorToast show={showErrorToast} message={errorMessage} />
